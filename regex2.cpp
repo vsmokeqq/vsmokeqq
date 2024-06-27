3fc5dd77405a77c89a4e6b37936ed932ba329674
vsmokeqq <alinkas83yandexby@gmail.com>
2024-06-28 00:48:23 +0300

    regex

---
+++ regex2.cpp
@@ -0,0 +1,18 @@
+#include <regex>
+#include <string>
+#include <iostream>
+#include <algorithm> 
+using namespace std;
+
+int main() {
+    string data;
+    cin >> data;
+
+    regex reg("a(b[^c]|[^b]c|[^b][^c])a");
+    auto pos = data.cbegin();
+    auto end = data.cend();
+    smatch m;
+    for (; regex_search(pos, end, m, reg); pos = m.suffix().first) {
+        cout << "match:  " << m.str() << endl;
+    }
+}
